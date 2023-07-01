ba4c2f5815a386d44d164c81e58299f9ae8b4922
anikethvarma <79694144+anikethvarma@users.noreply.github.com>
2023-07-02 00:30:32 +0530

    Create Set_Matrix_Zeros.py

---
+++ Set_Matrix_Zeros.py
@@ -0,0 +1,26 @@
+def setZeros(matrix: List[List[int]]) -> None:
+    m = len(matrix)
+    n = len(matrix[0])
+    first_row = False
+    first_col = False
+
+    for i in range(m):
+        for j in range(n):
+            if matrix[i][j] == 0:
+                matrix[0][j] = 0
+                matrix[i][0] = 0
+                if i == 0:
+                    first_row = True
+                if j == 0:
+                    first_col = True
+    
+    for i in range(1,m):
+        for j in range(1,n):
+            if matrix[i][0] == 0 or matrix[0][j] == 0:
+                matrix[i][j] = 0
+
+    if first_row:
+        matrix[0] = [0]*n
+    if first_col:
+        for i in range(m):
+            matrix[i][0] = 0
